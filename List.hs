2f8dde21e79985e1c5b8bc37640aa2daa253cca4
huyphung1602 <huy.phung.sw@gmail.com>
2021-04-06 08:52:01 +0700

    Add myButLast first solution

--- List.hs
+++ List.hs
@@ -1,9 +1,19 @@
 
-myLast :: [a] -> a
+-- Question 1: last
 -- pattern matching
+myLast :: [a] -> a
 myLast [] = error "No last for an empty list"
 myLast [x] = x
 myLast (_:xs) = myLast xs
 
 -- foldl1
--- myLast = foldl1 (\_ x -> x)
+myLast' :: [a] -> a
+myLast' = foldl1 (\_ x -> x)
+
+-- Question 2: My last but one element
+
+myButLast :: [a] -> a
+myButLast [] = error "No last but one element for an empty list"
+myButLast [x] = error "No last but one element singleton list"
+myButLast[x, _] = x
+myButLast (_:xs) = myButLast xs
