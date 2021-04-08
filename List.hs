e2e24aabe964191d26db62302830bd88872c91a8
huyphung1602 <huy.phung.sw@gmail.com>
2021-04-08 22:39:43 +0700

    Add solutions for Question 7

--- List.hs
+++ List.hs
@@ -61,3 +61,16 @@ isPalindrome' :: Eq a => [a] -> Bool
 isPalindrome' [] = True
 isPalindrome' [x] = True
 isPalindrome' xs = head xs == last xs && isPalindrome'(init $ tail xs)
+
+-- Question 7: Flatten a nested list structure.
+data NestedList a = Elem a | List [NestedList a] deriving (Show)
+
+flatten :: NestedList a -> [a]
+flatten (Elem x) = [x]
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+flatten (List []) = []
+
+flatten' :: NestedList a -> [a]
+flatten' (Elem x) = [x]
+-- flatten' (List xs) = foldr ((++) . flatten') [] xs
+flatten' (List xs) = concatMap flatten' xs
