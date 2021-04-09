fdd96ccfa1eec3b8f79089443d131a816d33ae0e
huyphung1602 <huy.phung.sw@gmail.com>
2021-04-10 00:35:43 +0700

    Refactor Question 16 and Question 17

--- src/ListCont.hs
+++ src/ListCont.hs
@@ -48,9 +48,15 @@ repli' xs k = concatMap (replicate k) xs
 
 -- Question 16: Drop every N'th element from a list.
 dropEvery :: (Eq k, Num k) => [a] -> k -> [a]
-dropEvery xs k = snd . foldl (\(a, b) x -> if a == k then (1, b) else (a + 1, b ++ [x])) (1, []) $ xs
+dropEvery xs k = snd . foldl dropFunc (1, []) $ xs
+  where dropFunc (a, b) x
+          | a == k = (1, b)
+          | otherwise = (a + 1, b ++ [x])
 
 -- Question 17: Split a list into two parts; the length of the first part is given.
 -- Do not use any predefined predicates.
 split :: [a] -> Int -> ([a], [a])
-split xs k = foldl (\(a, b) x -> if length a == k then (a, b ++ [x]) else (a ++ [x], b)) ([],[]) xs
+split xs k = foldl distribute ([],[]) xs
+  where distribute (a, b) x
+          | length a == k = (a, b ++ [x])
+          | otherwise = (a ++ [x], b)
