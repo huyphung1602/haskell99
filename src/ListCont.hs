615a7eb2504356b4f5b1fa6c85cf63cb07fed994
huyphung1602 <huy.phung.sw@gmail.com>
2021-04-10 00:50:45 +0700

    Add solution for Question 18

--- src/ListCont.hs
+++ src/ListCont.hs
@@ -60,3 +60,16 @@ split xs k = foldl distribute ([],[]) xs
   where distribute (a, b) x
           | length a == k = (a, b ++ [x])
           | otherwise = (a ++ [x], b)
+
+-- Question 18: (**) Extract a slice from a list.
+-- Given two indices, i and k, the slice is the list containing the elements between the i'th and k'th element of the original list (both limits included).
+-- Start counting the elements with 1.
+-- λ> slice ['a','b','c','d','e','f','g','h','i','k'] 3 7
+-- "cdefg"
+
+slice :: [a] -> Int -> Int -> [a]
+slice xs k j = reverse . snd . foldl appendFromTo (1, []) $ xs
+  where appendFromTo (a, b) x
+          | a < k = (a+1, b)
+          | a > j = (a+1, b)
+          | otherwise  = (a+1, x:b)
